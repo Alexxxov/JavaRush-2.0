2eedab58ce998d9e6d2f2188279ea68235b8f795
Alexander <aav94@yandex.ru>
2017-02-11 00:51:55 +0300

    task36.task3608 2 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task36.task3608.controller;
+
+import com.javarush.task.task36.task3608.model.Model;
+
+/**
+ * Created by Admin on 11.02.2017.
+ */
+public class Controller {
+    private Model model;
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void onShowAllUsers()
+    {
+        model.loadUsers();
+    }
+}
