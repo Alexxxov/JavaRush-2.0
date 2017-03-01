54052ed8996b469b7320ca0a10235009c6f20879
Alexander <aav94@yandex.ru>
2017-03-01 23:25:27 +0300

    task2909  14, 15 +

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,8 @@ public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
+    private Address address;
+    private boolean man;
 
     private Work work;
 
@@ -15,6 +13,14 @@ public class User {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public void printInfo()
@@ -54,24 +60,29 @@ public class User {
         this.age = age;
     }
 
+    public String getAddress() {
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
+    }
+
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
-    public String getAddress() {
-        return country + " " + city + " " + house.house;
+    public String getBoss()
+    {
+        return work.getBoss();
     }
 
     public Work getWork() {
