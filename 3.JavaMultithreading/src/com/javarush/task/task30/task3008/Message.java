e6133105d73d861e3b01c7c9c8d97ee44ac429ce
Alexander <aav94@yandex.ru>
2017-03-03 01:56:44 +0300

    task30 Chat 1 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Admin on 03.03.2017.
+ */
+public class Message {
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Admin on 03.03.2017.
+ */
+public enum MessageType {
+}
