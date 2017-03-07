08cc42c0a4c030085fef43d9d5340da06b804e48
Alexander <aav94@yandex.ru>
2017-03-08 02:44:28 +0300

    task30  7, 8 +

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,13 +3,18 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Admin on 03.03.2017.
  */
 public class Server {
 
-    public static void main(String[] args) {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args)
+    {
 
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
@@ -28,7 +33,18 @@ public class Server {
         }
     }
 
-
+    public static void sendBroadcastMessage(Message message)
+    {
+        for (Connection con :connectionMap.values())
+            try
+            {
+                con.send(message);
+            }
+            catch (IOException ioe)
+            {
+                ConsoleHelper.writeMessage("Exception occured, message cant be sent.");
+            }
+    }
 
     private static class Handler extends Thread
     {
@@ -36,6 +52,28 @@ public class Server {
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException
+        {
+            String clientName;
+            while (true)
+            {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message msg = connection.receive();
+                if (msg.getType() == MessageType.USER_NAME)
+                {
+                    clientName = msg.getData();
+                    if (clientName != null && !clientName.isEmpty())
+                        if (!(connectionMap.containsKey(clientName)))
+                        {
+                            connectionMap.put(clientName, connection);
+                            connection.send(new Message(MessageType.NAME_ACCEPTED));
+                            break;
+                        }
+                }
+            }
+            return clientName;
         }
     }
 
