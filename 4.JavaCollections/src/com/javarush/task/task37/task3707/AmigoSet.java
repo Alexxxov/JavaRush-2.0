d24001534fe4f5e772bff50cf815f94bc218a2c9
Alexander <aav94@yandex.ru>
2017-12-13 23:35:52 +0300

    big_34_AmigoSet task37.task3707 1, 2 done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * Created by Admin on 13.12.2017.
+ */
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        map = new HashMap<>(Math.max(16, (int)(collection.size() / 0.75f + 1)));
+        this.addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+
+    }
+
+    @Override
+    public boolean removeIf(Predicate filter) {
+        return false;
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return null;
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return null;
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
